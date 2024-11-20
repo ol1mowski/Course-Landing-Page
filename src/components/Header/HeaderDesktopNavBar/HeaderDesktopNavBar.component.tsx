4328dad14addf49cc77cf2ea297bd2c08f8fe34a
Oliwier <oliwier.markiewicz.dev@gmail.com>
2024-11-20 14:19:27 +0100

    Header: change navbar items design

--- src/components/Header/HeaderDesktopNavBar/HeaderDesktopNavBar.component.tsx
+++ src/components/Header/HeaderDesktopNavBar/HeaderDesktopNavBar.component.tsx
@@ -2,19 +2,19 @@ const HeaderDesktopNavBar = () => {
   return (
     <ul className="hidden lg:flex gap-10">
       <a href="#for-who">
-        <li className="text-xl transition-colors duration-500 hover:text-primary">Dla Kogo jest ten kurs ?</li>
+        <li className="text-lg transition-colors duration-500 hover:text-primary">Dla Kogo jest ten kurs ?</li>
       </a>
       <a href="#gain">
-        <li className="text-xl transition-colors duration-500 hover:text-primary">Co Otrzymasz ?</li>
+        <li className="text-lg transition-colors duration-500 hover:text-primary">Co Otrzymasz ?</li>
       </a>
       <a href="#bonus">
-        <li className="text-xl transition-colors duration-500 hover:text-primary">Bonus</li>
+        <li className="text-lg transition-colors duration-500 hover:text-primary">Bonus</li>
       </a>
       <a href="#offer">
-        <li className="text-xl transition-colors duration-500 hover:text-primary">Oferta</li>
+        <li className="text-lg transition-colors duration-500 hover:text-primary">Oferta</li>
       </a>
       <a href="#faq">
-        <li className="text-xl transition-colors duration-500 hover:text-primary">FAQ</li>
+        <li className="text-lg transition-colors duration-500 hover:text-primary">FAQ</li>
       </a>
     </ul>
   );
