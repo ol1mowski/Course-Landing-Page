950a7dc740edd3a341cb1fb10ba95017fb550e1f
Oliwier <oliwier.markiewicz.dev@gmail.com>
2024-11-23 11:43:58 +0100

    UI: add description component

--- src/components/Hero/HeroContent/HeroContent.component.tsx
+++ src/components/Hero/HeroContent/HeroContent.component.tsx
@@ -1,14 +1,15 @@
 import Button from "../../UI/Button/Button.component";
+import Description from "../../UI/Description/Description.component";
 
 const HeroContent = () => (
   <section className="flex flex-col gap-8">
     <h1 className="text-6xl font-bold leading-tight">
       Zostań programistą <br /> nawet w czasach kryzysu
     </h1>
-    <p className="w-2/3 flex text-lg font-light text-gray-500">
+    <Description>
       Dołącz do naszego ekskluzywnego kursu i zdobądź umiejętności, które
       otworzą przed Tobą nowe możliwości zawodowe!
-    </p>
+    </Description>
     <Button>Dowiedz się więcej</Button>
   </section>
 );
