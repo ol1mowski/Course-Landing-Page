5828f6f225737d1b12a3d3588101ddc9a6d6a8c0
Oliwier <oliwier.markiewicz.dev@gmail.com>
2024-11-24 19:12:58 +0100

    BlockSectionWave: add block image

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
-import Header from './components/Header/Header.component'
-import Hero from './components/Hero/Hero'
-import Excuses from './components/Excuses/Excuses'
+import Header from "./components/Header/Header.component";
+import Hero from "./components/Hero/Hero";
+import Excuses from "./components/Excuses/Excuses";
+import BlockSectionWave from "./components/BlockSectionWave/BlockSectionWave";
 
 function App() {
-
   return (
     <>
       <Header />
       <Hero />
       <Excuses />
+      <BlockSectionWave />  
     </>
   );
 }
 
-export default App
+export default App;
