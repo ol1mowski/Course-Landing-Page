646e7c6b2e2dd8da943e5705f11b10ef9f27c5b9
Oliwier <oliwier.markiewicz.dev@gmail.com>
2024-12-15 13:59:02 +0100

    Refactoring Footer component

---
+++ src/components/Footer/FooterBrand/FooterBrand.component.tsx
@@ -0,0 +1,22 @@
+import { motion } from "framer-motion";
+import { itemVariants } from "../../../animations/footerAnimations";
+
+const FooterBrand = () => (
+  <motion.div
+    variants={itemVariants}
+    custom={0}
+    className="col-span-1 lg:col-span-2"
+  >
+    <img
+      src="https://res.cloudinary.com/dbbuav0rj/image/upload/v1732100151/Course-site/site-logo_oyzuwi.svg"
+      alt="Logo"
+      className="h-12 mb-6 brightness-0 invert"
+    />
+    <p className="text-[#fefefe]/90 max-w-md">
+      Rozpocznij swoją przygodę z programowaniem już dziś. Dołącz do
+      naszej społeczności i rozwijaj się razem z nami.
+    </p>
+  </motion.div>
+);
+
+export default FooterBrand; 
