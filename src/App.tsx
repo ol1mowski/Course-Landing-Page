48fe71ca51f0831e4281ef7740dc18fca3db0b5c
Oliwier <oliwier.markiewicz.dev@gmail.com>
2024-12-16 06:28:53 +0100

    Route: add new /login route

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import PaymentPage from "./routes/Payment/Payment.component";
 import MainLayout from "./layouts/MainLayout";
 import HomePage from "./pages/HomePage";
 import Panel from "./routes/Panel/Panel.component";
+import Login from "./routes/Login/Login.component";
 
 function App() {
   return (
@@ -17,6 +19,7 @@ function App() {
           <Route index element={<HomePage />} />
           <Route path="/platnosc" element={<PaymentPage />} />
           <Route path="/panel" element={<Panel />} />
+          <Route path="/login" element={<Login />} />
         </Route>
       </Routes>
     </BrowserRouter>
