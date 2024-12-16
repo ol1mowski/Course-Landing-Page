faa09be537bc32a3efa31ecab12efa1452b9144e
Oliwier <oliwier.markiewicz.dev@gmail.com>
2024-12-16 06:22:23 +0100

    tests: add e2e test to check orderForm validation

--- src/components/Payment/OrderSummary/PaymentButton/PaymentButton.component.tsx
+++ src/components/Payment/OrderSummary/PaymentButton/PaymentButton.component.tsx
@@ -6,6 +6,7 @@ type PaymentButtonProps = {
 
 const PaymentButton = ({ amount }: PaymentButtonProps) => (
   <button
+    data-testid="payment-button"
     type="submit"
     className="w-full bg-primary text-white py-3 px-6 rounded-lg hover:bg-primary/90 transition-colors duration-300 mt-6"
   >
