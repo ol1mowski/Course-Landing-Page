f74b59ce4d59a937760f2aa366fea26ae5d2bf23
Oliwier <oliwier.markiewicz.dev@gmail.com>
2024-12-19 07:26:15 +0100

    Lessons: remove unnecessary margin

--- src/components/Lessons/Lessons.component.tsx
+++ src/components/Lessons/Lessons.component.tsx
@@ -6,7 +6,6 @@ import SectionItem from "./SectionItem/SectionItem.component";
 
 const Lessons = () => {
   return (
-    <section className="mt-96 relative top-96 pb-96">
       <SectionWrapper>
         <Heading>Spis lekcji zawartych w <span className="text-primary">kursie</span></Heading>
         <div className="flex flex-col gap-4 w-full max-w-4xl">
@@ -20,7 +19,6 @@ const Lessons = () => {
           ))}
         </div>
       </SectionWrapper>
-    </section>
   );
 };
 
