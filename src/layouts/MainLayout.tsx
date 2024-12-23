31b6a7b52041aa9115c45fa39f154c28129bd385
Oliwier <oliwier.markiewicz.dev@gmail.com>
2024-12-23 22:16:05 +0100

    dashboard: Add specialized header for dashboard pages

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,14 +1,16 @@
 import { Outlet, useLocation } from "react-router-dom";
 import Footer from "../components/Footer/Footer.component";
 import Header from "../components/Header/Header.component";
+import DashboardHeader from "../components/DashboardHeader/DashboardHeader.component";
 
 const MainLayout = () => {
   const location = useLocation();
   const hideHeader = location.pathname === '/platnosc';
+  const isDashboard = location.pathname.startsWith('/mojekonto');
 
   return (
     <>
-      {!hideHeader && <Header />}
+      {!hideHeader && (isDashboard ? <DashboardHeader /> : <Header />)}
       <Outlet />
       <Footer />
     </>
