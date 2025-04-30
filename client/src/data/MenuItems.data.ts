ef7f9a7af5d669b99f60e01316cc1d67f4860ebb
Ol1mowski <olimarki123@wp.pl>
2025-04-30 07:56:08 +0200

    feat(Header section): change items content

--- client/src/data/MenuItems.data.ts
+++ client/src/data/MenuItems.data.ts
@@ -5,6 +5,11 @@ export type MenuItem = {
 };
 
 export const MENU_ITEMS: MenuItem[] = [
-  { id: 1, label: "Panel", href: "/mojekonto" },
-  { id: 2, label: "Moje dane", href: "/mojekonto/dane" },
+  { id: 1, label: "O kursie", href: "#o-kursie" },
+  { id: 2, label: "Dla kogo jest kurs?", href: "#dla-kogo-jest-kurs" },
+  { id: 3, label: "Korzyści z kursu", href: "#korzysci-z-kursu" },
+  { id: 4, label: "Czego się nauczysz?", href: "#czego-sie-nauczysz" },
+  { id: 5, label: "O autorze", href: "#o-autorze" },
+  { id: 6, label: "FAQ", href: "#faq" },
+  { id: 7, label: "Oferta", href: "#oferta" },
 ];
